0268533f11bd4141138764a56b61a8640a516802
Chaitanya <chaitanyakaushal9@gmail.com>
2026-05-27 20:09:39 +0530

    switch frontend to bearer-token auth with global axios interceptor

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,12 +1,13 @@
 import { Navbar } from '@/components/layout/navbar';
 import { Footer } from '@/components/layout/footer';
+import { RequireAuth } from '@/components/auth/require-auth';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <RequireAuth>
       <Navbar />
       <div className="flex flex-1 flex-col">{children}</div>
       <Footer />
-    </>
+    </RequireAuth>
   );
 }
